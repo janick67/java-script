6e24550ceea90dcd6d4fbc45ff84e237f827bebf
Jan Worwa <worwajan67@gmail.com>
2019-03-09 17:24:46 +0100

    dodane jeszcze kolejne tabelki

--- nodejs/api.js
+++ nodejs/api.js
@@ -22,6 +22,99 @@ db.connect((err) => {
   console.log('MySql Connected...');
 });
 
+
+app.get('/api/wydatki/query',(req,res) => {
+  response(req,res, obj => {});
+});
+
+//------------------------------------------------------------------------------------------------
+//SELECT sum(kwota) FROM `wydatki` WHERE bank = 'PKO' or bank = 'BGZ' or bank = 'OPT' or bank = 'GOT' or bank = 'MBA' or bank = 'DOM' or bank = 'inne'
+app.get('/api/wydatki/saldo/query',(req,res) => {
+  response(req,res, obj => {
+    obj.select = 'sum(kwota)';
+  });
+});
+
+  // `SELECT Year(data), MONTH(data), sum(kwota) FROM wydatki WHERE kogo = 'moje' GROUP BY Year(data), MONTH(data) order by Year(data), MONTH(data)`;
+app.get('/api/wydatki/saldo_na_miesiac/query',(req,res) => {
+  response(req,res, obj => {
+    if (typeof obj.select === 'undefined') obj.select = '';
+    if (typeof obj.where === 'undefined') obj.where = '';
+    if (typeof obj.groupby === 'undefined') obj.groupby = '';
+    if (typeof obj.orderby === 'undefined') obj.orderby = 'Year(data), MONTH(data)';
+    obj.select += 'Year(data), MONTH(data), sum(kwota)';
+    obj.where += 'and kogo = "moje"';
+    obj.groupby += 'Year(data), MONTH(data)';
+  });
+});
+
+//SELECT * FROM `wydatki` WHERE powiązane = 0 and kogo not like 'inne' and kogo not like 'moje' and data > '2018-09-01'
+app.get('/api/wydatki/kto_ma_oddac/query',(req,res) => {
+  response(req,res, obj => {
+    if (typeof obj.where === 'undefined') obj.where = '';
+    if (typeof obj.orderby === 'undefined') obj.orderby = 'kogo';
+    obj.where += 'and powiązane = 0 and kogo not like "inne" and kogo not like "moje" and data > "2018-09-01"';
+  });
+});
+
+//SELECT kogo, sum(kwota) FROM `wydatki` WHERE powiązane = 0 and kogo not like 'inne' and kogo not like 'moje' and data > '2018-09-01' group by kogo
+app.get('/api/wydatki/kto_ma_oddac_suma/query',(req,res) => {
+  response(req,res, obj => {
+    if (typeof obj.select === 'undefined') obj.select = '';
+    if (typeof obj.where === 'undefined') obj.where = '';
+    if (typeof obj.groupby === 'undefined') obj.groupby = '';
+    if (typeof obj.orderby === 'undefined') obj.orderby = 'Year(data), MONTH(data)';
+    obj.select += 'kogo, sum(kwota)';
+    obj.where += 'and powiązane = 0 and kogo not like "inne" and kogo not like "moje" and data > "2018-09-01"';
+    obj.groupby += 'kogo';
+  });
+});
+
+
+function generujSql(obj){
+  if (typeof obj.select === 'undefined') obj.select = '*';
+
+  if (typeof obj.where === 'undefined') {
+    obj.where = '';
+  }else{
+    obj.where = 'where 1 ' + obj.where;
+  }
+
+  if (typeof obj.orderby === 'undefined' || obj.orderby.length < 2) {
+    obj.orderby = '';
+  }else{
+    obj.orderby = 'order by ' + obj.orderby;
+  }
+
+  if (typeof obj.groupby === 'undefined') {
+    obj.groupby = '';
+  }else{
+    obj.groupby = 'group by ' + obj.groupby;
+  }
+
+  if (typeof obj.limit === 'undefined') {
+    obj.limit = '';
+  }else{
+    obj.limit = 'limit ' + obj.limit;
+  }
+
+  if (typeof obj.offset === 'undefined'){
+    obj.offset = '';
+  }else{
+    obj.offset = 'offset ' + obj.offset;
+  }
+
+  if (typeof obj.from === 'undefined') obj.from = 'wydatki';
+
+  let sql = `SELECT ${obj.select} from ${obj.from} ${obj.where} ${obj.groupby} ${obj.orderby} ${obj.limit} ${obj.offset}`;
+  console.log(sql);
+  return sql;
+}
+
+app.listen(3000, () => console.log('Listen on port 3000...'))
+
+
+
 function response(req,res,func)
 {
   if (req.query.table !== "wydatki") return res.send("wybrano zła tabele");
@@ -68,72 +161,3 @@ function decodeWhere(obj)
     }
   return where;
 }
-
-
-app.get('/api/wydatki/query',(req,res) => {
-  response(req,res, obj => {});
-});
-
-//------------------------------------------------------------------------------------------------
-//SELECT sum(kwota) FROM `wydatki` WHERE bank = 'PKO' or bank = 'BGZ' or bank = 'OPT' or bank = 'GOT' or bank = 'MBA' or bank = 'DOM' or bank = 'inne'
-app.get('/api/wydatki/saldo/query',(req,res) => {
-  response(req,res, obj => {
-    obj.select = 'sum(kwota)';
-  });
-});
-
-app.get('/api/wydatki/saldo_na_miesiac/query',(req,res) => {
-  // `SELECT Year(data), MONTH(data), sum(kwota) FROM wydatki WHERE kogo = 'moje' GROUP BY Year(data), MONTH(data) order by Year(data), MONTH(data)`;
-  response(req,res, obj => {
-    if (typeof obj.select === 'undefined') obj.select = '';
-    if (typeof obj.where === 'undefined') obj.where = '';
-    if (typeof obj.groupby === 'undefined') obj.groupby = '';
-    if (typeof obj.orderby === 'undefined') obj.orderby = 'Year(data), MONTH(data)';
-    obj.select += 'Year(data), MONTH(data), sum(kwota)';
-    obj.where += 'and kogo = "moje"';
-    obj.groupby += 'Year(data), MONTH(data)';
-  });
-});
-
-
-function generujSql(obj){
-  if (typeof obj.select === 'undefined') obj.select = '*';
-
-  if (typeof obj.where === 'undefined') {
-    obj.where = '';
-  }else{
-    obj.where = 'where 1 ' + obj.where;
-  }
-
-  if (typeof obj.orderby === 'undefined' || obj.orderby.length < 2) {
-    obj.orderby = '';
-  }else{
-    obj.orderby = 'order by ' + obj.orderby;
-  }
-
-  if (typeof obj.groupby === 'undefined') {
-    obj.groupby = '';
-  }else{
-    obj.groupby = 'group by ' + obj.groupby;
-  }
-
-  if (typeof obj.limit === 'undefined') {
-    obj.limit = '';
-  }else{
-    obj.limit = 'limit ' + obj.limit;
-  }
-
-  if (typeof obj.offset === 'undefined'){
-    obj.offset = '';
-  }else{
-    obj.offset = 'offset ' + obj.offset;
-  }
-
-  if (typeof obj.from === 'undefined') obj.from = 'wydatki';
-
-  let sql = `SELECT ${obj.select} from ${obj.from} ${obj.where} ${obj.groupby} ${obj.orderby} ${obj.limit} ${obj.offset}`;
-  console.log(sql);
-  return sql;
-}
-
-app.listen(3000, () => console.log('Listen on port 3000...'))

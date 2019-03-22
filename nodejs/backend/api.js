32d9fbe310142a032ba51e98d9b841a50f811f7d
Jan Worwa <worwajan67@gmail.com>
2019-03-22 19:42:33 +0100

    poprawki po przeniesieniu

--- nodejs/backend/api.js
+++ nodejs/backend/api.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(__dirname + '/frontend/'));
+app.use(express.static('../frontend/'));
 
 const data = new Date();
 

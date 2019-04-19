bdb7cbb7ddaa61cd9a19c6c17ae69672da48b640
Jan Worwa <worwajan67@gmail.com>
2019-04-19 11:44:02 +0200

    przepisanie na bootstrapa przyciskow i nawigacji

--- nodejs/frontend/js/main.js
+++ nodejs/frontend/js/main.js
@@ -3,6 +3,12 @@ const uri = document.location.origin + "/";
 let aktualna_tabelka = null;
 let distinct = new Distinct();
 
+
+setTimeout(function () {
+  distinct.init();
+  console.log("elo");
+}, 1000);
+
 //-----------------------------------------MAIN----------------------------------
 const tabelka = new Tabelka("wydatki","wydatki");
 tabelka.adres += 'api/'+tabelka.sql_table+"/query";

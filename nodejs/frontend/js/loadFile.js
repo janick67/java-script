31be1f2b6dfb944d1370571d8cc2de9e7a771b04
Jan Worwa <worwajan67@gmail.com>
2019-08-29 18:04:39 +0200

    usuwanie elementów lf

--- nodejs/frontend/js/loadFile.js
+++ nodejs/frontend/js/loadFile.js
@@ -12,7 +12,7 @@ function handleFileSelect(evt) {
     
   
     var reader = new FileReader();
-    reader.readAsText(f, 'UTF-8');
+    reader.readAsText(f,'Windows-1250'); //'Windows-1250' mbank i pko     dbfree - utf-8
     reader.onprogress = function(evt) {
     };
     reader.onload = function(evt) {
@@ -26,10 +26,26 @@ function handleFileSelect(evt) {
           data = res;
         }
       }else if(f.extension == '.csv'){
+        
+        if (result.slice(0,5) == 'mBank') {
+          console.log('mbank');
+          data = csvObj(result.slice(result.indexOf('#Data operacji;')),';');
+        }else{
         data = csvObj(result);
+        }
       }else{
         data = result;
       }
+      //65533 - błędne kodowanie
+      
+      // data.forEach(row =>{
+      //   Object.getOwnPropertyNames(row).forEach(el =>{
+      //     row[el] = row[el].replace(/["']/gm,'');
+      //   })
+      // })
+
+      //console.log(data);
+      console.log(JSON.stringify(data).replace(/\\r/gm, '').replace(/\\"/gm, ''));
       
       first(data);
 
@@ -39,35 +55,64 @@ function handleFileSelect(evt) {
     };
   }
 
+  let makeATable = (data)=>{
+    let tab = document.createElement('table');
+    let trHead = document.createElement('tr');   
+    trHead.classList.add('trHead');
+    for (const el in data[0]){
+        let th = document.createElement('th');
+        th.innerText = el;
+        trHead.append(th);
+    }
+    tab.append(trHead); 
+
+    let tbody = document.createElement('tbody');
+    data.forEach((row, i) => {
+      let tr = document.createElement('tr');
+      for (const col in row){
+          let td = document.createElement('td');
+          let span = document.createElement('span');
+          span.innerText = row[col];
+          td.setAttribute('col',col);
+          td.append(span);
+          tr.setAttribute('row',i);
+          tr.append(td);
+      }
+      tbody.append(tr);
+    })
+    tab.append(tbody);
+    return tab;
+  }
+
   let first = (data)=>{
     $('#modal_load_file').modal('show');
-      let tab = document.createElement('table');
-      let trHead = document.createElement('tr');   
-      for (const el in data[0]){
-          let th = document.createElement('th');
-          th.innerText = el;
-          trHead.append(th);
-      }
-      tab.append(trHead); 
+      let tab = makeATable(data);
 
-      let tbody = document.createElement('tbody');
-      data.forEach(row => {
-        let tr = document.createElement('tr');
-        for (const e in row){
-            let td = document.createElement('td');
-            td.innerText = row[e];
-            tr.append(td);
-        }
-        tbody.append(tr);
-      })
-      tab.append(tbody);
       document.querySelector('#lfTable').innerHTML='';
       document.querySelector('#lfTable').append(tab);
+
+      tab.onclick = e =>{
+        let {target} = e;
+        if (target.tagName === 'TR'){
+          if(e.offsetX < 0){
+            let row =  target.getAttribute('row');
+            console.log(target, row);
+            data = data.splice(row,1);  
+
+            target.addEventListener('transitionend', function() {
+              console.log('transitionend fired', typeof target); 
+              target.remove();
+            });
+            target.classList.add('hide');
+          }
+        }
+      }
 
       let tab2 = document.createElement('table');
       tab2.classList.add('selectsTable');
       let trHead2 = document.createElement('tr');
       let tr2 =document.createElement('tr');
+      let tr2Input =document.createElement('tr');
 
       getJson('/api/wydatki/columns')
       .then(resp => {
@@ -85,38 +130,64 @@ function handleFileSelect(evt) {
             th2.innerText = col;
             trHead2.append(th2);
             let td2 = document.createElement('td');
+            let td2Input = document.createElement('td');
             let select = document.createElement('select');
             select.classList.add('form-control');
-            let option = document.createElement('option');
-                option.innerText = '';
-                option.value = '';
-                select.append(option);
-            for (const el in data[0]){
+            let createOption = text =>{
                 let option = document.createElement('option');
-                option.innerText = el;
-                option.value = el;
-                select.append(option);
+                option.innerText = text;
+                option.value = text;
+                return option;
+            }
+            select.append(createOption(''));
+            for (const el in data[0]){
+              select.append(createOption(el));
             } 
+            select.append(createOption('Inne...'));
             select.setAttribute('for',col);
             if (typeof matched[col].file != 'undefined') select.value = matched[col].file;
+            let input = document.createElement('input');
+            input.setAttribute('for',col);
+            input.classList.toggle('hide');
+            input.classList.add('form-control');
+            td2Input.append(input);
             td2.append(select);
             tr2.append(td2);
+            tr2Input.append(td2Input);
           })
           tab2.append(trHead2);
-          tab2.append(tr2);   
+          tab2.append(tr2);  
+          tab2.append(tr2Input);   
           document.querySelector('#lfSelects').innerHTML='';
           document.querySelector('#lfSelects').append(tab2);
           document.querySelector('#lfNext').onclick = (e)=>{
             document.querySelectorAll('#lfSelects select').forEach((el)=>{
               matched[el.getAttribute('for')].select = el.value;
             });
+            document.querySelectorAll('#lfSelects input').forEach((el)=>{
+              
+              matched[el.getAttribute('for')].input = el.value;
+            });
             second(data,matched);
           }
       })
 
+      tab2.onchange = e =>{        
+        if (e.target.tagName == 'SELECT'){
+          let atr = e.target.getAttribute('for');
+          let table = getClossestTag(e.target,'table');
+          if (e.target.value == 'Inne...'){
+            table.querySelector(`input[for=${atr}]`).classList.remove('hide');
+          }else{
+            table.querySelector(`input[for=${atr}]`).classList.add('hide');
+          }
+        }
+      }
   }
 
   let second = (data,columns) => {
+    console.log(data, columns);
+    
     let lfBody = document.querySelector('#lfBody');
     firstScreanHTML = lfBody.innerHTML;    
     $('#modal_load_file').on('hide.bs.modal',()=>{
@@ -128,8 +199,12 @@ function handleFileSelect(evt) {
       data.forEach(row => {
         let obj = {};
         for (const el in columns){
-          let se = columns[el].select;          
-             obj[el] = row[se];
+          let se = columns[el].select;    
+          if (se == 'Inne...'){
+            obj[el] = columns[el].input;
+          } else{
+            obj[el] = (typeof row[se] != 'undefined') ? row[se] : '';
+          }
         }
         readyData.push(obj);
       })
@@ -137,35 +212,20 @@ function handleFileSelect(evt) {
       readyData = data;
     }
 
-    let tab = document.createElement('table');
-      let trHead = document.createElement('tr');   
-      for (const el in readyData[0]){
-          let th = document.createElement('th');
-          th.innerText = el;
-          trHead.append(th);
-      }
-      tab.append(trHead); 
-
-      let tbody = document.createElement('tbody');
-      readyData.forEach((row,i) => {
-        let tr = document.createElement('tr');
-        for (const col in row){
-            let td = document.createElement('td');
-            td.setAttribute('row',i);
-            td.setAttribute('col',col);
-            td.innerText = row[col];
-            tr.append(td);
-        }
-        tbody.append(tr);
-      })
+    let tab = makeATable(readyData);
+    let tbody = tab.querySelector('tbody');
+    console.log(tbody);
+    
       tbody.ondblclick = e =>{
-        let el = e.target;
+        console.log(e,getClossestTag(e.target,'TR'));
+        
+        let el = getClossestTag(e.target,'TD');
         if (el.tagName == 'TD'){
           let input = document.createElement('input');
           input.value = el.innerText;
           el.innerHTML = '';
           input.onfocusout = e =>{
-            let row = e.target.parentNode.getAttribute('row');
+            let row = getClossestTag(e.target,'tr').getAttribute('row');
             let col = e.target.parentNode.getAttribute('col');
             readyData[row][col] = e.target.value;
             e.target.parentNode.innerHTML = e.target.value;
@@ -174,9 +234,10 @@ function handleFileSelect(evt) {
           input.focus();
         }
       }
-      tab.append(tbody);
+      
       document.querySelector('#lfTable').innerHTML='';
       document.querySelector('#lfTable').append(tab);
+
       document.querySelector('#lfSubmit').onclick = e =>{
         let errors = 0;
         readyData.forEach((row,i) => {
@@ -185,7 +246,7 @@ function handleFileSelect(evt) {
           for (const el in row){ // pętla po wszystkich kolumnach
           if (typeof err[el] === 'undefined') { // jeśli nie ma błędow dla tego elementu to znaczy ze wszystko jest ok
           }else{
-            let elem = document.querySelector(`td[row="${i}"][col="${el}"]`);
+            let elem = document.querySelector(`tr[row="${i}"] td[col="${el}"]`);
             elem.classList.add('error');
             elem.title = err[el][0];
           }
@@ -194,6 +255,7 @@ function handleFileSelect(evt) {
       if (errors > 0)
        console.log("Znaleziono następującą liczbę błędów: "+errors);
       else {
+        console.log(JSON.stringify(readyData));
         send_insert(uri+"api/wydatki",readyData).then(res=>{
           let err = res.errors;
           let errorRows = [];
@@ -254,12 +316,12 @@ function handleFileSelect(evt) {
     return el1 == el2; 
   }
   
-  function csvObj(csv){
+  function csvObj(csv,char = ','){
     var lines=csv.split("\n");
     var result = [];
     for(var i=1;i<lines.length;i++){
         var obj = {};
-        var currentline=lines[i].split(",");
+        var currentline=lines[i].split(char);
         for(var j=0;j<currentline.length;j++){
         obj[j] = currentline[j];
         }
@@ -279,3 +341,5 @@ function handleFileSelect(evt) {
   let dropZone = document.querySelector('#modal_add .modal-dialog');
   dropZone.addEventListener('dragover', handleDragOver, false);
   dropZone.addEventListener('drop', handleFileSelect, false);
+
+
